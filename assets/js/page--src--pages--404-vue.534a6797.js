1f3b0e7617895ed99be22e23e60189b9624dae30
uche <thesimplearchitect@gmail.com>
2020-10-20 01:56:25 +0200

    Blog redesign

---
+++ assets/js/page--src--pages--404-vue.534a6797.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{"7l0S":function(t,n,e){"use strict";e.r(n);var l={metaInfo:{title:"404"}},o=e("KHd+"),s=Object(o.a)(l,(function(){var t=this.$createElement,n=this._self._c||t;return n("Layout",[n("h1",[this._v("404")]),n("p",[this._v("Not found")])])}),[],!1,null,null,null);n.default=s.exports}}]);

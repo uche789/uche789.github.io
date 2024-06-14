d725bac0fc4d6ff6c19820d31ef81269c1308441
uche789 <3889297+uche789@users.noreply.github.com>
2024-06-14 20:14:16 +0200

    Fix new planner app

--- projects/planner/registerSW.js
+++ projects/planner/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./projects/plannersw.js', { scope: './projects/planner' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}

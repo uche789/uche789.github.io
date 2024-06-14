975494986dc0e5da035e85ecc6ac6db850c7dd76
uche789 <3889297+uche789@users.noreply.github.com>
2024-06-14 20:38:01 +0200

    Fix new planner app

--- projects/planner/assets/CloseIcon-D37-OCq0.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as t,c as o,a as s}from"./index-5N3f8waN.js";const c={},n={xmlns:"http://www.w3.org/2000/svg",height:"48",viewBox:"0 -960 960 960",width:"48"},_=s("path",{d:"m249-207-42-42 231-231-231-231 42-42 231 231 231-231 42 42-231 231 231 231-42 42-231-231-231 231Z"},null,-1),a=[_];function r(d,h){return t(),o("svg",n,a)}const l=e(c,[["render",r]]);export{l as default};
---
+++ projects/planner/assets/FilterIcon-Oig9rd7o.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as o,a as s}from"./index-uv1MocEO.js";const c={},n={xmlns:"http://www.w3.org/2000/svg",height:"24",viewBox:"0 -960 960 960",width:"24"},_=s("path",{d:"M400-240v-80h160v80H400ZM240-440v-80h480v80H240ZM120-640v-80h720v80H120Z"},null,-1),r=[_];function a(h,i){return t(),o("svg",n,r)}const v=e(c,[["render",a]]);export{v as default};
